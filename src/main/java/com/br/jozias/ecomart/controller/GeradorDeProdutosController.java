c9dd4222a3ab4d3bc9b136f54ac06b91d5927313
Jozias <aelciojoziasp@gmail.com>
2025-07-28 00:00:13 -0300

    exemplo de configurar parametros

--- src/main/java/com/br/jozias/ecomart/controller/GeradorDeProdutosController.java
+++ src/main/java/com/br/jozias/ecomart/controller/GeradorDeProdutosController.java
@@ -1,6 +1,8 @@
 package com.br.jozias.ecomart.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,11 @@ public class GeradorDeProdutosController {
     private final ChatClient chatClient;
 
     public GeradorDeProdutosController(ChatClient.Builder chatClientBuilder) {
+        chatClientBuilder.defaultOptions(OpenAiChatOptions.builder()
+                        .model(OpenAiApi.ChatModel.CHATGPT_4_O_LATEST)
+                        .temperature(2.0)
+                        .maxTokens(100)
+                .build());
         this.chatClient = chatClientBuilder.build();
     }
 
